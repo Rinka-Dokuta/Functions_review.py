8a024712911d24ea27156ee95eb80652c7af0f7a
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-03-18 09:55:42 -0600

    Create functions_review.py

---
+++ functions_review.py
@@ -0,0 +1,34 @@
+def calculate_distance():
+    print("Input the speed.")
+    speed = int(input())
+    print("Speed>", speed, "Km/h")
+    
+    print("Input the time.")
+    time = int(input())
+    print ("Time>", time, "hours.")
+    
+    product = speed * time
+    
+    print("The distance is", product, "km.")
+    
+
+
+calculate_distance()
+    
+    
+def calculate_area_of_rectangle():
+    print("Input the length.")
+    length = int(input())
+    print ("Length>", length)
+    
+    print("Input the width.")
+    width = int(input())
+    print ("Width>", width)
+    
+    product = length * width
+    
+    print("The area is", product)
+    
+calculate_area_of_rectangle()
+    
+    
